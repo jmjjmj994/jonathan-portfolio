a41e6739d82745e23773c048d0154b54dfbd4c39
jmjjmj994 <jonathan.mjos@outlook.com>
2023-06-09 23:32:19 +0200

    Created links

--- utilities.js
+++ utilities.js
@@ -4,4 +4,5 @@ export const hamburger = document.querySelector(".hamburger");
 export const projectImg1 = document.querySelector(".scrolling-image-1");
 export const projectImg2 = document.querySelector(".scrolling-image-2");
 export const navItem = document.querySelectorAll(".nav-item");
+export const lightMode = document.querySelector(".light-mode");
 
